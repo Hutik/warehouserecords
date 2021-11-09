2e9dfb076090868093a59cacc5521a1fd7c2e99c
Hutik <mariusz10081997@gmail.com>
2021-11-09 17:52:27 +0100

    first commit

---
+++ src/main/java/pl/kowalewski/warehouserecords/WarehouserecordsApplication.java
@@ -0,0 +1,13 @@
+package pl.kowalewski.warehouserecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouserecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouserecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kowalewski/warehouserecords/WarehouserecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kowalewski.warehouserecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouserecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
