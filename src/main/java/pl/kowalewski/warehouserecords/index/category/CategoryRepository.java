1f3ba9779ca182fa722972c7767b8730ee71ebb1
Hutik <mariusz10081997@gmail.com>
2023-01-15 21:21:10 +0100

    Cosmetic code changes

--- src/main/java/pl/kowalewski/warehouserecords/index/category/CategoryRepository.java
+++ src/main/java/pl/kowalewski/warehouserecords/index/category/CategoryRepository.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
     List<Category> findAll();
     Optional<Category> findById(Long id);
